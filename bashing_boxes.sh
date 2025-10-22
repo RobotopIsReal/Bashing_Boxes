be2ef479f2c3b92ec2e58464c9cbbf1476988129
RobotopIsReal <nasim.alikishibekov@gmail.com>
2025-10-22 14:54:15 -0500

    Updated the script further, added a functional print

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,2 +1,13 @@
-items=("Cologne" "Crown" "Guitar" "Banana" "Milkshake" "Globe" "Raisin" "Cone" "Stocking" "Chart")
-echo "${ITEMS[1]}"
+items=10
+
+list=("Cologne" "Crown" "Guitar" "Banana" "Milkshake" "Globe" "Raisin" "Cone" "Stocking" "Chart")
+echo "${list[1]}" # The $ indicates that 'items' isn't just text, but rather a value.
+read -p "Select an input.
+1 - Print list
+2 - Print item at X position in list
+3 - Add item to list
+4 - Remove last item from list
+5 - Remove item from X position
+6 - Exit
+" userinput
+echo "$userinput"
