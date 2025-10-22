9aa057e18954ba77c8388b8a07568aff9c9550d0
RobotopIsReal <nasim.alikishibekov@gmail.com>
2025-10-22 10:23:49 -0500

    Began work on the script

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,1 +1,2 @@
-items = ["Cologne", "Crown", "Guitar", "Banana", "Milkshake", "Globe", "Raisin", "Cone", "Stocking", "Chart"]
+items=("Cologne" "Crown" "Guitar" "Banana" "Milkshake" "Globe" "Raisin" "Cone" "Stocking" "Chart")
+echo "${ITEMS[1]}"
